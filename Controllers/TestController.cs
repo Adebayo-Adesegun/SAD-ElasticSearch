3b9e71b75b4d235bfe269d53e96d974eb77e2c48
Adebayo Adesegun Daniel <segun@glovaro.com>
2022-01-11 10:54:36 +0100

    Setup Elastic Client Configurations for connection and Indexing

---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SAD_ElasticSearch.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAD_ElasticSearch.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IElasticSearch _elasticSearch;
+
+       public TestController(IElasticSearch elasticSearch)
+        {
+            _elasticSearch = elasticSearch;
+        }
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SAD_ElasticSearch.Core.Interfaces;
+using SAD_ElasticSearch.Infrastructure.ElasticSearch;
 
 namespace SAD_Api
 {
@@ -30,7 +25,24 @@ namespace SAD_Api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SAD_Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { 
+                    Title = "Smart Apartment Data", 
+                    Description = "Smart Data AWS Opensearh assessment",
+                    Version = "v1" 
+                });
+            });
+
+            services.AddSingleton<IElasticSearch, ElasticSearchService>();
+
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .SetIsOriginAllowed((host) => true)
+                        .AllowAnyHeader());
             });
         }
 
@@ -43,6 +55,8 @@ namespace SAD_Api
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SAD_Api v1"));
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseHttpsRedirection();
 
